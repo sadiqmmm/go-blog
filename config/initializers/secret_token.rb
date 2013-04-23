773d0c64cb42b9160552868a135a01569d100bc1
mohammed sadiq <sadiqmmm@gmail.com>
2013-04-23 18:43:25 +0530

    first blogger commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '1b0b186fba6927d9e96883a359ca30d0a8bc3dda1716637c2858d0a9a0a994802a0141933c4e5b2abfc086199984c991f0d4ba063597c12426016b5f65fea201'
